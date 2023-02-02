a4d0d57ffff3ab2a1482acdf0679b9455858196d
Robert C. Haye <roberthaye1994@gmail.com>
2023-02-02 16:33:57 +0100

    setup redux store

--- src/components/CalendarGridHeader/style/CalendarGridHeader-Style.ts
+++ src/components/CalendarGridHeader/style/CalendarGridHeader-Style.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const DaysContainer = styled.div`
+  align-items: center;
   text-align: center;
   border: 1px solid #ddd;
+  background-color: #628269;
+  height: 50px;
 `;
--- src/components/CalendarHeader/CalendarHeader.tsx
+++ src/components/CalendarHeader/CalendarHeader.tsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { ArrowButton } from "./style/CalendarHeader-style";
+import moment from "moment";
 
 const CalendarHeader = ()=>{
-     /**
+  // Variables: 
+  const  year = moment(0).format("YYYY")
+  const monthAndYear = moment(0).format("MMMM") + " " + year;
+
+  const [curMonth, setCurMonth] = useState(0);
+  /**
    * Function - handle upating month on click
    */
-   const [curMonth, setCurMonth] = useState(0);
   const handleLeftArrow = ()=> {
     setCurMonth(curMonth - 1)
     }
@@ -15,11 +20,13 @@ const CalendarHeader = ()=>{
     setCurMonth(curMonth + 1)
     }
     
-    return (<>
-       <ArrowButton onClick={handleLeftArrow} type="submit">
-       <FontAwesomeIcon icon={faAngleLeft} /> 
-    </ArrowButton>
-    <ArrowButton onClick={handleRigthArrow}  type="submit"> <FontAwesomeIcon icon={faAngleRight} /></ArrowButton>
+    return (
+    <>
+      <h2>{monthAndYear}</h2>
+      <ArrowButton onClick={handleLeftArrow} type="submit">
+      <FontAwesomeIcon icon={faAngleLeft} /> 
+      </ArrowButton>
+      <ArrowButton onClick={handleRigthArrow}  type="submit"> <FontAwesomeIcon icon={faAngleRight} /></ArrowButton>
     </>)
 }
 export default CalendarHeader;
