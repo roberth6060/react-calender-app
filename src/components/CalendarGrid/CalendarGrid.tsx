abcc840793d83e9880f916df8d8745a57a43d66b
Robert C. Haye <roberthaye1994@gmail.com>
2023-02-03 00:54:48 +0100

    update days grid style

--- src/components/CalendarGrid/CalendarGrid.tsx
+++ src/components/CalendarGrid/CalendarGrid.tsx
@@ -1,21 +1,22 @@
 import { render } from "@testing-library/react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import moment from "moment";
+import { CalendarDates, CalendarEvent, CalendarNumber } from "./style/CalendarGrid-Style";
 
 export const CalendarGrid = ()=> {
       const {curMonth, curDate} = useAppSelector((state)=> state.calendar);
    const curDays =  Array.from(Array(moment(curDate).daysInMonth()).keys());
    console.log(curDays)
-  return (<>
+  return (<CalendarDates>
     {curDays.map((curDay)=> {
         return( <button key={curDay}
 		className="calendar-date ${type}-date"
 		data-day="${number}"
 		data-date="${date}"
 		title="${date}">
-		    <span className="calendar-number">{curDay}</span>
-		    <span className="calendar-events"></span>
+		    <CalendarNumber>{curDay + 1}</CalendarNumber>
+		    <CalendarEvent></CalendarEvent>
 		</button>)
     })}
-    </>)
+    </CalendarDates>)
 }
---
+++ src/components/CalendarGrid/style/CalendarGrid-Style.ts
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+export const CalendarDates = styled.div`
+  background: #ddd;
+  grid-column-gap: 1px;
+  grid-row-gap: 1px;
+  border-left: 1px solid #ddd;
+  border-right: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  height: 35rem;
+
+  & > * {
+    position: relative;
+    background: #fff;
+    overflow: hidden;
+    z-index: 1;
+  }
+
+  & > *:focus {
+    outline: 1px solid #6772e5;
+  }
+
+  & > *::before {
+    content: "";
+    display: block;
+    padding-top: 100%;
+  }
+
+  & > .prev-date span {
+    opacity: 0.5;
+  }
+
+  & > .next-date span {
+    opacity: 0.5;
+  }
+
+  & > .today-date {
+    background: #dff1de;
+  }
+
+  & > .calendar-active {
+    background: #dddffb;
+  }
+
+  #calendar-active {
+    text-align: right;
+    opacity: 0.5;
+  }
+`;
+
+export const CalendarNumber = styled.span`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  line-height: 1;
+  z-index: 1;
+`;
+
+export const CalendarEvent = styled.span`
+  position: absolute;
+  top: 35px;
+  left: 0;
+  right: 0;
+  font-size: 0.875rem;
+  white-space: nowrap;
+  text-align: left;
+
+  & > * {
+    display: block;
+    background: #6772e5;
+    color: #fff;
+    margin: 1px 2px;
+    padding: 2px 4px;
+    border-radius: 0.25rem;
+  }
+`;
