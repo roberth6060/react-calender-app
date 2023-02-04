a5db7800186bc8323e0c4ea4011e30f05725b8ed
Robert C. Haye <roberthaye1994@gmail.com>
2023-02-04 12:24:25 +0100

    sucessfully connected to mangodb and graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,8 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
+import mongoose, { connect, ConnectOptions } from "mongoose";
 import * as dotenv from "dotenv";
-import mongoose from "mongoose";
 
 //Resolvers
 import { EventResolver } from "./resolvers/event.resolvers";
@@ -17,29 +17,27 @@ const executeMain = async () => {
     validate: false,
   });
 
-  const MONGO_URI = process.env.MANGODB_URI;
-  if (!MONGO_URI) {
-    throw new Error("You must provide a MongoLab URI");
-  }
+  mongoose.set("strictQuery", false);
 
-  mongoose.Promise = global.Promise;
-  mongoose.connect(MONGO_URI);
-  mongoose.connection
-    .once("open", () => console.log("Connected to MongoLab instance."))
-    .on("error", (error) =>
-      console.log("Error connecting to MongoLab:", error)
-    );
+  const mongooseConnect = await connect(`${process.env.MANGODB_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions);
+
+  await mongooseConnect.connection;
 
   const server = new ApolloServer({ schema: schema });
   const expressServer: Express.Application = Express();
 
-  server.applyMiddleware({ app: expressServer });
+  server.start().then((_) => {
+    server.applyMiddleware({ app: expressServer });
 
-  expressServer.listen({ port: process.env.PORT }, () =>
-    console.log(
-      `Server ready and listening at ==> http://localhost:${process.env.PORT}${server.graphqlPath}`
-    )
-  );
+    expressServer.listen({ port: process.env.PORT }, () =>
+      console.log(
+        `Server ready and listening at ==> http://localhost:${process.env.PORT}${server.graphqlPath}`
+      )
+    );
+  });
 };
 
 executeMain().catch((error) => {
