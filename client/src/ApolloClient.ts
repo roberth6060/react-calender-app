ce1c6a0e70625c2f28d3815573b1ebfa27885bd9
Robert C. Haye <roberthaye1994@gmail.com>
2023-02-06 14:11:29 +0100

    added temp graphql api hosted on heroku

--- client/src/ApolloClient.ts
+++ client/src/ApolloClient.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: "/graphql",
+  // uri: "/graphql",
+  uri: "https://calendar-web-app.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
